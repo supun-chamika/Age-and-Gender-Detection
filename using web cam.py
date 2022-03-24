119200ba90017b19d084fc1e7d9d7ec98e5dfe17
supun-chamika <101165810+supun-chamika@users.noreply.github.com>
2022-03-24 16:01:44 +0530

    Add files via upload

---
+++ using web cam.py
@@ -0,0 +1,170 @@
+# Import Libraries
+import cv2
+import numpy as np
+
+# The gender model architecture
+GENDER_MODEL = 'weights/deploy_gender.prototxt'
+
+# The gender model pre-trained weights
+GENDER_PROTO = 'weights/gender_net.caffemodel'
+
+# Each Caffe Model impose the shape of the input image also image preprocessing is required like mean
+# substraction to eliminate the effect of illunination changes
+MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+
+# Represent the gender classes
+GENDER_LIST = ['Male', 'Female']
+
+FACE_PROTO = "weights/deploy.prototxt.txt"
+FACE_MODEL = "weights/res10_300x300_ssd_iter_140000_fp16.caffemodel"
+
+# The model architecture
+AGE_MODEL = 'weights/deploy_age.prototxt'
+
+# The model pre-trained weights
+AGE_PROTO = 'weights/age_net.caffemodel'
+
+# Represent the 8 age classes of this CNN probability layer
+AGE_INTERVALS = ['(0, 2)', '(4, 6)', '(8, 12)', '(15, 20)', '(25, 32)', '(38, 43)', '(48, 53)', '(60, 100)']
+
+# Initialize frame size
+frame_width = 1280
+frame_height = 720
+
+# load face Caffe model
+face_net = cv2.dnn.readNetFromCaffe(FACE_PROTO, FACE_MODEL)
+
+# Load age prediction model
+age_net = cv2.dnn.readNetFromCaffe(AGE_MODEL, AGE_PROTO)
+
+# Load gender prediction model
+gender_net = cv2.dnn.readNetFromCaffe(GENDER_MODEL, GENDER_PROTO)
+
+
+def get_faces(frame, confidence_threshold=0.5):
+    # convert the frame into a blob to be ready for NN input
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104, 177.0, 123.0))
+    # set the image as input to the NN
+    face_net.setInput(blob)
+    # perform inference and get predictions
+    output = np.squeeze(face_net.forward())
+    # initialize the result list
+    faces = []
+    # Loop over the faces detected
+    for i in range(output.shape[0]):
+        confidence = output[i, 2]
+        if confidence > confidence_threshold:
+            box = output[i, 3:7] * \
+                  np.array([frame.shape[1], frame.shape[0],
+                            frame.shape[1], frame.shape[0]])
+            # convert to integers
+            start_x, start_y, end_x, end_y = box.astype(np.int)
+            # widen the box a little
+            start_x, start_y, end_x, end_y = start_x - \
+                                             10, start_y - 10, end_x + 10, end_y + 10
+            start_x = 0 if start_x < 0 else start_x
+            start_y = 0 if start_y < 0 else start_y
+            end_x = 0 if end_x < 0 else end_x
+            end_y = 0 if end_y < 0 else end_y
+            # append to our list
+            faces.append((start_x, start_y, end_x, end_y))
+    return faces
+
+
+# from: https://stackoverflow.com/questions/44650888/resize-an-image-without-distortion-opencv
+def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+    # resize the image
+    return cv2.resize(image, dim, interpolation=inter)
+
+
+def get_gender_predictions(face_img):
+    blob = cv2.dnn.blobFromImage(
+        image=face_img, scalefactor=1.0, size=(227, 227),
+        mean=MODEL_MEAN_VALUES, swapRB=False, crop=False
+    )
+    gender_net.setInput(blob)
+    return gender_net.forward()
+
+
+def get_age_predictions(face_img):
+    blob = cv2.dnn.blobFromImage(
+        image=face_img, scalefactor=1.0, size=(227, 227),
+        mean=MODEL_MEAN_VALUES, swapRB=False
+    )
+    age_net.setInput(blob)
+    return age_net.forward()
+
+
+def predict_age_and_gender():
+    """Predict the gender of the faces showing in the image"""
+    # create a new cam object
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        _, img = cap.read()
+        # Take a copy of the initial image and resize it
+        frame = img.copy()
+        # resize if higher than frame_width
+        if frame.shape[1] > frame_width:
+            frame = image_resize(frame, width=frame_width)
+        # predict the faces
+        faces = get_faces(frame)
+        # Loop over the faces detected
+        # for idx, face in enumerate(faces):
+        for i, (start_x, start_y, end_x, end_y) in enumerate(faces):
+            face_img = frame[start_y: end_y, start_x: end_x]
+            # predict age
+            age_prediction = get_age_predictions(face_img)
+            # predict gender
+            gender_prediction = get_gender_predictions(face_img)
+            i = gender_prediction[0].argmax()
+            gender = GENDER_LIST[i]
+            gender_confidence_score = gender_prediction[0][i]
+            i = age_prediction[0].argmax()
+            age = AGE_INTERVALS[i]
+            age_confidence_score = age_prediction[0][i]
+            # Draw the box
+            label = f"{gender}-{gender_confidence_score * 100:.1f}%, {age}-{age_confidence_score * 100:.1f}%"
+            # label = "{}-{:.2f}%".format(gender, gender_confidence_score*100)
+            print(label)
+            y_pos = start_y - 15
+            while y_pos < 15:
+                y_pos += 15
+            box_color = (255, 0, 0) if gender == "Male" else (147, 20, 255)
+            cv2.rectangle(frame, (start_x, start_y), (end_x, end_y), box_color, 2)
+            # Label processed image
+            cv2.putText(frame, label, (start_x, y_pos),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.54, box_color, 2)
+
+            # Display processed image
+        cv2.imshow("Gender Estimator", frame)
+        if cv2.waitKey(1) == ord("q"):
+            break
+        # save the image
+        cv2.imwrite("output.jpg", frame)
+    # Cleanup
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    predict_age_and_gender()
